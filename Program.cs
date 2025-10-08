8e82672f7b2ac9582a8b4e04765b827cf1ac3773
DarinDmitry1 <dima.darin.2023@gmail.com>
2025-10-08 10:15:47 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tasks tasks = new Tasks();
+
+        tasks.FractionTask();
+        tasks.CharToNumTask();
+        tasks.Is2DigitsTask();
+        tasks.IsInRangeTask();
+        tasks.IsEqualTask();
+        tasks.AbsTask();
+        tasks.Is35Task();
+        tasks.Max3Task();
+        tasks.Sum2Task();
+        tasks.DayTask();
+        tasks.ListNumsTask();
+        tasks.ChetTask();
+        tasks.NumLenTask();
+        tasks.SquareTask();
+        tasks.RightTriangleTask();
+        tasks.FindFirstTask();
+        tasks.MaxAbsTask();
+        tasks.AddTask();
+        tasks.ReverseBackTask();
+        tasks.FindAllTask();
+    }
+}
